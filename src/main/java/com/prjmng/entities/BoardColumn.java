d32eccfd14b5ddc438f4d358a7242aa03492198f
NizoDotNet <mmmdovnizami.official@gmail.com>
2026-06-21 14:09:17 +0400

    minor changes

--- src/main/java/com/prjmng/entities/BoardColumn.java
+++ src/main/java/com/prjmng/entities/BoardColumn.java
@@ -1,10 +1,7 @@
 package com.prjmng.entities;
 
-import com.prjmng.entities.enums.SprintStatus;
 import jakarta.persistence.*;
 import lombok.*;
-
-import java.time.LocalDate;
 import java.util.UUID;
 
 @Entity
